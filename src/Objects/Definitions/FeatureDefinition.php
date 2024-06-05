bb63ba5b0949854ae33e5b3b7235d105c9bade95
jxtou <jxtou@xirelogy.com>
2024-06-05 14:23:21 +0800

    Bugfix: respect FeatureEntryDefault

--- src/Objects/Definitions/FeatureDefinition.php
+++ src/Objects/Definitions/FeatureDefinition.php
@@ -2,11 +2,13 @@
 
 namespace MagpieLib\Cerberus\Objects\Definitions;
 
+use MagpieLib\Cerberus\Annotations\FeatureEntryDefault;
 use MagpieLib\Cerberus\Concepts\Configs\DefinitionCreatable;
 use MagpieLib\Cerberus\Concepts\Objects\CommonFeature;
 use MagpieLib\Cerberus\Impl\Config;
 use MagpieLib\Cerberus\Objects\Adapters\FeatureByDefinition;
 use MagpieLib\Cerberus\Objects\Feature;
+use ReflectionAttribute;
 
 /**
  * Feature definable by reflection
@@ -25,6 +27,23 @@ class FeatureDefinition extends BaseDefinition implements CommonFeature
     public function isDefaultProvided() : bool
     {
         return $this->isDefaultProvided;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    protected function onAcceptAttribute(ReflectionAttribute $attribute) : void
+    {
+        parent::onAcceptAttribute($attribute);
+
+        switch ($attribute->getName()) {
+            case FeatureEntryDefault::class:
+                $this->isDefaultProvided = true;
+                break;
+            default:
+                break;
+        }
     }
 
 
